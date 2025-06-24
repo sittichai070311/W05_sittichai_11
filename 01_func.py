8654c2feedd2dbcb1282e429eca68ac841fa75ac
sittichai070311 <hrangfah070311@gmail.com>
2025-06-24 16:49:29 +0700

    first commit

---
+++ 01_func.py
@@ -0,0 +1,18 @@
+'''def ชื่อฟังก์ชั่น(parameter):
+
+'''
+# ไม่รับพารา
+def greeting_eng():
+    print("Hello World!!!")
+    print("Hi")
+    
+def greeting_th():
+    print("ว่าไง!+")
+    print("สวัสดีชาวโลกกกกกกก")
+
+def get_time():
+    from datetime import datetime
+    now = datetime.now()
+    print(now)
+
+get_time()
